32a11386704c44632c2d7dbd29ad4021555ceed7
Lucas Ribeiro <lucashenriqueribeirosilva@gmail.com>
2026-09-01 11:55:14 -0300

    configura URL da API de produção

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://personal-trainer-api-ileo.onrender.com/api'
 });
 
 export const alunoService = {
